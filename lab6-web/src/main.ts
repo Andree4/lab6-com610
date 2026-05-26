150747b7105c2e9186b398856e15187c2aa82aa5
Andree <daniel.arancibia40o0@gmail.com>
2026-05-25 22:18:14 -0400

    feat(ci): automatic deploy

--- lab6-web/src/main.ts
+++ lab6-web/src/main.ts
@@ -4,8 +4,8 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <div class="card">
-    <h1>Lab 6 web</h1>
-    <input type="file" id="fileInput" accept="image/*" />
+    <h1>Lab 6 web - deploy automatico</h1>
+    <input class="file-upload" type="file" id="fileInput" accept="image/*" />
     <div id="preview"></div>
   </div>
 `;
